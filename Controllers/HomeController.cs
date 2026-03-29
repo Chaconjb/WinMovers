3a67d0dfaf7c22460529de8688bff94f91ad0684
Yulianaaf <yulianaaguilarf@gmail.com>
2026-03-28 20:55:11 -0600

    HTML de winmovers

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WinMovers.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Página principal
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Página de privacidad (opcional)
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WinMovers.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
